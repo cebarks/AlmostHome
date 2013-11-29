b4334701a5d7a7b04232e33208d66e50102518be
Anten Skrabec <cebarks@gmail.com>
2013-11-29 13:36:52 -0700

    Removed RealLocation tag from Debug Menu

--- src/main/java/net/cebarks/ahome/gfx/gui/GuiDebug.java
+++ src/main/java/net/cebarks/ahome/gfx/gui/GuiDebug.java
@@ -1,7 +1,6 @@
 package net.cebarks.ahome.gfx.gui;
 
 import net.cebarks.ahome.AlmostHome;
-import net.cebarks.ahome.entity.EntityPlayer;
 import net.cebarks.ahome.level.Level;
 
 public class GuiDebug extends GuiBase {
@@ -15,13 +14,11 @@ public class GuiDebug extends GuiBase {
 	public void render() {
 		if (game.isDebug() || true) {
 			AlmostHome a = level.getGame();
-			EntityPlayer p = level.getPlayer();
 
 			if (level.getGame().getTickCount() % 50 == 0)
 				usedMemory = (Runtime.getRuntime().maxMemory() - Runtime.getRuntime().freeMemory()) / 1048576;
 
 			g.drawString("FPS: " + a.getFPS() + ", TPS: " + a.getTPS(), 10, 20);
-			g.drawString("RL: " + p.x + ", " + p.y, 10, 35);
 			g.drawString("Mem: " + usedMemory + "mb / " + Runtime.getRuntime().maxMemory() / 1048576 + "mb", 10, 50);
 			g.drawString("TILE: " + level.getCurrentLevelTile().gridX + ", " + level.getCurrentLevelTile().gridY, 10, 65);
 			g.drawString("B: " + level.getCurrentLevelTile().getPopulator(), 10, 80);
